425371e8f8015edca2b07b70294348170f66a4f0
karolSobkowicz <99karolsobkowicz@gmail.com>
2019-11-04 14:56:35 +0000

    ignore unnecessary folders, add test

---
+++ src/textbookExercises/ObjectOrientedThinking/BMITest.java
@@ -0,0 +1,12 @@
+package textbookExercises.ObjectOrientedThinking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMITest {
+
+    @org.junit.jupiter.api.Test
+    void getBMI() {
+        BMI bmiOwen = new BMI("Owen Silvester", 19, 160, 80);
+        assertEquals(17.58, bmiOwen.getBMI());
+    }
+}
